8d3d992a1f82f1cfa652f12b7ae98325853f3042
aurelienamat <aurelien.amat@sfr.fr>
2026-02-12 09:04:06 +0100

    Inscription

--- server.js
+++ server.js
@@ -18,11 +18,35 @@ connection.connect((err) => {
   console.log('Connecté à la base de données MySQL.');
 });
 
-app.use(express.static('html'));
+app.use(express.static('html')); //Selection du dossier html
 app.use(express.json()); //Sert a utiliser json
 
 
 //Lire sur le port 3000
 app.listen(3000, () => {
     console.log('Server is running on :3000');
+})
+
+
+//Gestion Inscription Utilisateur
+app.post('/inscription', (req,res) => {
+  console.log(req.body);
+  //Conditions insertion
+  
+  //Hachage mot de passe 
+  
+  //Insertion dans la base
+  connection.query(
+    'INSERT INTO users(nom,prenom,email,password,classe) VALUES(?,?,?,?,?)',
+    [req.body.nom,req.body.prenom,req.body.email,req.body.password,req.body.classe],
+    (err,results) => {
+      if(err){
+        console.log('Erreur Insertion dans la base' + err);
+        res.status(500).json({mesage : 'Erreur serveur'});
+        return;
+      }
+      console.log('Insertion réussi');
+      res.json({message : 'Inscription reussie !'});
+    }
+  )
 })
