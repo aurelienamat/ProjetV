fbb2d6babf28576f4949a191ec711296443bc36c
aurelienamat <aurelien.amat@sfr.fr>
2026-02-12 10:20:47 +0100

    Connexion

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-
-const mysql = require('mysql2');
+const bcrypt = require('bcrypt'); //POUR HASH
+const mysql = require('mysql2'); //Mysql
 
 //Connexion a la base de donner
 const connection = mysql.createConnection({
-    host:'172.29.18.133',
-    user:'Ticket',
-    password:'passwordticket',
-    database:'TICKETS'
+  host: '172.29.18.133',
+  user: 'Ticket',
+  password: 'passwordticket',
+  database: 'TICKETS'
 });
 connection.connect((err) => {
   if (err) {
@@ -24,29 +24,69 @@ app.use(express.json()); //Sert a utiliser json
 
 //Lire sur le port 3000
 app.listen(3000, () => {
-    console.log('Server is running on :3000');
+  console.log('Server is running on :3000');
 })
 
 
 //Gestion Inscription Utilisateur
-app.post('/inscription', (req,res) => {
+app.post('/inscription', (req, res) => {
   console.log(req.body);
   //Conditions insertion
-  
+
   //Hachage mot de passe 
-  
-  //Insertion dans la base
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      //Insertion dans la base
+      connection.query(
+        'INSERT INTO users(nom,prenom,email,password,classe) VALUES(?,?,?,?,?)',
+        [req.body.nom, req.body.prenom, req.body.email, hash, req.body.classe],
+        (err, results) => {
+          if (err) {
+            console.log('Erreur Insertion dans la base' + err);
+            res.status(500).json({ mesage: 'Erreur serveur' });
+            return;
+          }
+          console.log('Insertion réussi');
+          res.json({ message: 'Inscription reussie !' });
+        }
+      )
+    })
+
+})
+
+//CONNEXION
+app.post('/connexion', (req, res) => {
+  console.log(req.body);
+
+  //Récupération password dans la base pour la comparaison
   connection.query(
-    'INSERT INTO users(nom,prenom,email,password,classe) VALUES(?,?,?,?,?)',
-    [req.body.nom,req.body.prenom,req.body.email,req.body.password,req.body.classe],
-    (err,results) => {
-      if(err){
-        console.log('Erreur Insertion dans la base' + err);
-        res.status(500).json({mesage : 'Erreur serveur'});
+    'SELECT password,id FROM users WHERE email = ?',
+    [req.body.email], (err, results) => {
+      if (err) {
+        console.log("Erreur récupération email " + err);
         return;
       }
-      console.log('Insertion réussi');
-      res.json({message : 'Inscription reussie !'});
+      if (results.length == 0) {
+        console.log("Erreur identifiant");
+        res.json({ message: 'Identifiant ou mot de passe invalides' });
+        return;
+      }
+      console.log(results[0]);
+      //res.json({ message: 'Email trouvé' });
+      let resultat = results[0];
+      bcrypt.compare(req.body.password, resultat.password, (err, results) => {
+        if(err){
+          console.log('Erreur compare' + err);
+          return;
+        }
+        if(results){
+          console.log('Connexion réussi');
+          res.json({id : resultat.id});
+        }else{
+          res.json({message : 'connexion echoué'});
+          return;
+        }
+      })
     }
   )
 })
