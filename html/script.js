e57610828b7a741b8a4e251cd6b13767e2c249a1
aurelienamat <aurelien.amat@sfr.fr>
2026-03-04 15:35:53 +0100

    Separation fichier

--- html/script.js
+++ html/script.js
@@ -1,34 +1,4 @@
 
-// Containers des pages
-const loginContainer = document.querySelector('.loginContainer');
-const ticketContainer = document.getElementById('container-Ticket');
-const tpcontainer = document.getElementById('container-tp');
-const avcontaineur = document.getElementById('containeur-avancement');
-
-// Boutons de navigation
-const btnConnexionInscription = document.getElementById('connexion-inscription');
-const btnTicket = document.getElementById('btn-Ticket');
-const btnTp = document.getElementById('btn-Tp');
-const btnAvancement = document.getElementById('btn-Avancement');
-
-// formulaire connexion/inscription
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const prenom = document.getElementById('prenom');
-const nom = document.getElementById('nom');
-const classe = document.getElementById('classe');
-
-// Boutons du formulaire
-const connexion = document.getElementById('submit-btn');
-const btnInscription = document.getElementById('btnInscription');
-const signupField = document.querySelector('.signupField');
-
-
-const btnClose = document.getElementById('close-phone');
-const btnOpen = document.getElementById('open-phone');
-const navComputer = document.getElementById('nav-computer');
-
-const btnCreateTicket = document.getElementById('submit-btn-create-ticket');
 // ROUTES ---------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // ROUTE INSCRIPTION ========================================================
@@ -381,8 +351,6 @@ window.addEventListener('resize', () => {
 
 
 // GESTION DES BOUTONS NAVIGATION ACTIVE
-
-const navButtons = document.querySelectorAll('#navigation li');
 
 navButtons.forEach(btn => {
     btn.addEventListener('click', () => {
---
+++ html/variable.js
@@ -0,0 +1,33 @@
+
+// Containers des pages
+const loginContainer = document.querySelector('.loginContainer');
+const ticketContainer = document.getElementById('container-Ticket');
+const tpcontainer = document.getElementById('container-tp');
+const avcontaineur = document.getElementById('containeur-avancement');
+
+// Boutons de navigation
+const btnConnexionInscription = document.getElementById('connexion-inscription');
+const btnTicket = document.getElementById('btn-Ticket');
+const btnTp = document.getElementById('btn-Tp');
+const btnAvancement = document.getElementById('btn-Avancement');
+
+// formulaire connexion/inscription
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const prenom = document.getElementById('prenom');
+const nom = document.getElementById('nom');
+const classe = document.getElementById('classe');
+
+// Boutons du formulaire
+const connexion = document.getElementById('submit-btn');
+const btnInscription = document.getElementById('btnInscription');
+const signupField = document.querySelector('.signupField');
+
+
+const btnClose = document.getElementById('close-phone');
+const btnOpen = document.getElementById('open-phone');
+const navComputer = document.getElementById('nav-computer');
+
+const btnCreateTicket = document.getElementById('submit-btn-create-ticket');
+
+const navButtons = document.querySelectorAll('#navigation li');
